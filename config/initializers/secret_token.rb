cc2d91f8dc42b1bc0443dfb18ffc6eea21afe3bf
RajendranAmmu <rajendranrails@gmail.com>
2012-09-29 15:51:26 +0530

    ffff

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RajuApp::Application.config.secret_token = 'a512895ee8382761f1cd6d29adef1206e71c5ed8ec69d3cc8a09a0f3c19a75639e1d3ff53e45f963b581008e4190d2cea9c64f834fe1418ed04d7f2a621729d8'
